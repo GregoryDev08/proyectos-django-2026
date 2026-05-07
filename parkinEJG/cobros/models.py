ac59f0417279bb697c7e38f051a304cc262b2bbd
JhonnGV1219 <jhoonnguanoluisa@gmail.com>
2026-05-07 16:30:06 -0500

    Se agrego detalles en la clase Cliente

--- parkinEJG/cobros/models.py
+++ parkinEJG/cobros/models.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.db import models
 
-# Create your models here.
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100, verbose_name="Nombre")
